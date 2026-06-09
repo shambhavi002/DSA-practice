f6fe6cf96bb9ca3e6a0793f5e828f832c5a49791
Shambhavi Gupta <guptashambhavi022@gmail.com>
2026-06-09 12:18:18 +0530

    Create bubble sort.cpp

---
+++ Sorting/bubble sort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void bubbleSort(int arr[], int n){
+for(int i=0; i<n-1; i++){
+for(int j=0; j<n-i-1; j++){
+if(arr[j]>arr[j+1]){
+int temp=arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
+}
+}
+}
+}
+int main(){
+int brr[4]={3,6,89,45};
+bubbleSort(brr,4);
+cout<<"Sorted "<<endl;
+for(int i=0; i<4; i++){
+cout<<brr[i]<<endl;
+}
+}
