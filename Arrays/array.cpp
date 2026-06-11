6e1d017ba2fa1a1f76887a7254802059b61266ab
Shambhavi Gupta <guptashambhavi022@gmail.com>
2026-06-11 22:29:28 +0530

    Create array.cpp

---
+++ Arrays/array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[]={9,7,8,6,5};
+int n;
+cin>>n;
+for(int i=0; i<n; i++){
+cout<<arr[i]<<endl;
+}
+char ary[]={'m','b','h'};
+int m;
+cin>>m;
+for(char i=0; i<m; i++){
+cout<<ary[i]<<endl;
+}
+}
