5f8cb3bc2582bab523e777ab0c3a4968dbd18109
Shambhavi Gupta <guptashambhavi022@gmail.com>
2026-06-11 22:35:46 +0530

    Create array scope.cpp

---
+++ Arrays/array scope.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void update(int arr[], int n){
+cout<<"inside function"<<endl;
+arr[0]=120;
+for(int i=0; i<2; i++){
+cout<<arr[i]<<" ";
+}
+cout<<endl;
+cout<<"going back to main function"<<endl;
+}
+int main(){
+int arr[2]={0,1};
+update(arr,2);
+for(int i=0; i<3; i++){
+cout<<arr[i]<<endl;
+}
+cout<<endl;
+}
+//avoid using update in array as it changes the original array
