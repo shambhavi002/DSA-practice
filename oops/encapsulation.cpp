ad6164123b737ea1c853893209744d70360bef5f
Shambhavi Gupta <guptashambhavi022@gmail.com>
2026-06-12 16:48:24 +0530

    Create encapsulation.cpp

---
+++ oops/encapsulation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//Encapsulation
+class Student{
+private:
+string name;
+int age;
+int height;
+public:
+int getAge(){
+return this->age;
+}
+};
+int main(){
+Student first;
+cout<<"alright "<<endl;
+}
+
