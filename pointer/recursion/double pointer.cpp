454ef12a6ab04d8732b28e35bf3d0ab6a24efdbb
Shambhavi Gupta <guptashambhavi022@gmail.com>
2026-06-21 13:49:26 +0530

    Add double pointer example with update function

---
+++ pointer/recursion/double pointer.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+void update(int **p){
+p=p+1;
+*p=*p+1;
+**p=**p+1;
+
+}
+int main(){
+int i=5;
+int*p=&i;
+int **p2=&p;
+cout<<"Alright"<<endl;
+cout<<"value of p "<<p<<endl;
+cout<<"adress of p "<<&p<<endl;
+cout<<*p2<<endl;
+cout<<i<<endl;
+cout<<*p<<endl;
+cout<<**p2<<endl;
+cout<<&i<<endl;
+cout<<p<<endl;
+cout<<*p2<<endl;
+cout<<&p<<endl;
+cout<<p2<<endl;
+cout<<"before "<<i<<endl;
+cout<<"before "<<p<<endl;
+cout<<"before "<<p2<<endl;
+update(p2);  
+cout<<"after "<<p2<<endl;
+
+
+
+}
