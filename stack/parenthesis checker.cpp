0f5c29794c3875dfdfc51d25694fde2bf17be1ab
Shambhavi Gupta <guptashambhavi022@gmail.com>
2026-06-30 18:40:33 +0530

    Add parenthesis checker using stack
    Implement a function to check if parentheses are balanced.

---
+++ stack/parenthesis checker.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool isBalanced(string& s) {
+   
+    stack<char> st; 
+   
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        }
+        
+        else if (c == ')' || c == '}' || c == ']') {
+            
+            // No opening bracket
+            if (st.empty()) return false; 
+            char top = st.top();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                return false;
+            }
+            
+            // Pop matching opening bracket
+            st.pop(); 
+        }
+    }
+    
+    // Balanced if stack is empty
+    return st.empty(); 
+}
+
+int main() {
+    string s="[()()]{}";
+    cout<<(isBalanced(s)?"true":"false");
+    return 0;
+}
